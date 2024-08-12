78489fe0af827e6fee5cae5cab76fc019dd03b44
ken789tar <174225250+ken789tar@users.noreply.github.com>
2024-08-12 13:02:10 +0900

    config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Traveler
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.encoding = "utf-8"
+    Encoding.default_external = Encoding::UTF_8
   end
 end
